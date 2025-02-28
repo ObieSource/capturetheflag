8eabd5d8a1c724f83a881d9fc396208892b9bce1
souffle17 <144641936+souffle17@users.noreply.github.com>
2025-02-28 04:59:14 -0500

    add favicon page

---
+++ src/app/empty/page.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: 'Custom Page',
+  description: 'This is a custom page with a unique favicon',
+  icons: {
+    icon: '/../qrcode.ico', // Custom favicon for this page
+  },
+};
+
+export default function Empty() {
+  return (
+    <div>
+      <h1>Nothing to see here...</h1>
+    </div>
+  );
+}
